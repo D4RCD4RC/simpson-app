c80edf78a9349eb0e99f9194bf436f7ccfa04092
Daniel Rivas <193283886+D4nielRiv4s@users.noreply.github.com>
2025-11-29 22:31:52 +0100

    Crear pipe para manejar las imágenes de los personajes.

--- src/app/personaje/components/tarjeta-personaje/tarjeta-personaje.ts
+++ src/app/personaje/components/tarjeta-personaje/tarjeta-personaje.ts
@@ -1,20 +1,14 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Personaje } from '../../interfaces/personaje.interface';
 import { SlicePipe } from '@angular/common';
 import { RouterLink } from "@angular/router";
-import { environment } from '../../../../environments/environment';
+import { PersonajeImagePipe } from '../../pipes/personaje-image-pipe';
 
 @Component({
   selector: 'tarjeta-personaje',
-  imports: [SlicePipe, RouterLink],
+  imports: [SlicePipe, RouterLink, PersonajeImagePipe],
   templateUrl: './tarjeta-personaje.html',
 })
 export class TarjetaPersonaje {
   personaje = input.required<Personaje>();
-
-  imageUrl = computed(() => {
-    const id = this.personaje().id;
-    return `${environment.baseUrlImage}/character/${id}.webp`;
-  });
-
 }
---
+++ src/app/personaje/pipes/personaje-image-pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+
+@Pipe({
+  name: 'personajeImage',
+})
+export class PersonajeImagePipe implements PipeTransform {
+
+  transform(path: string | null | undefined): string {
+
+    // Si no existe → usar imagen del public
+    if (!path || path.trim().length === 0) {
+      return '/no-image.webp';
+    }
+
+    // Si ya es URL completa → retornar
+    if (path.startsWith('http')) return path;
+
+    // Si empieza con / → concatenar
+    if (path.startsWith('/')) {
+      return `${environment.baseUrlImage}${path}`;
+    }
+
+    // Si viene sin "/" → caso raro
+    return `${environment.baseUrlImage}/${path}`;
+  }
+
+}
