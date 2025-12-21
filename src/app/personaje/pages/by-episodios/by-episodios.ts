10684b8975a29dc9afaba88573ae4f43fae26bdb
Daniel Rivas <193283886+D4nielRiv4s@users.noreply.github.com>
2025-12-21 21:27:56 +0100

    Paginar episodios

--- src/app/personaje/pages/by-episodios/by-episodios.ts
+++ src/app/personaje/pages/by-episodios/by-episodios.ts
@@ -3,20 +3,25 @@ import { TarjetaEpisodio } from "../../../episodios/components/tarjeta-episodio/
 import { EpisodiosService } from '../../../episodios/services/episodio.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { BuscarPersonaje } from "../../../shared/components/buscar-personaje/buscar-personaje";
+import { Pagination } from "../../../shared/components/pagination/pagination";
+import { PaginationService } from '../../../shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'by-episodios',
-  imports: [TarjetaEpisodio, BuscarPersonaje],
+  imports: [TarjetaEpisodio, BuscarPersonaje, Pagination],
   templateUrl: './by-episodios.html',
 })
 export class ByEpisodios {
 
   episodiosService = inject(EpisodiosService);
+  paginationService = inject(PaginationService);
 
   episodiosResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage() }),
     stream: ({ params }) => {
-      return this.episodiosService.getEpisodios({})
+      return this.episodiosService.getEpisodios({
+        page: params.page,
+      })
     }
   })
 
